230d014641ef73145dc8e96aa2fc5ec08a443265
humblEgo <injun.woo30000@gmail.com>
2021-05-14 03:05:52 +0900

    refactor: Router에 path에 맞는 컨트롤러 찾는 책임 넘겨줌

--- app/src/main/java/com/codesoom/assignment/httpHandlers/TaskHttpHandler.java
+++ app/src/main/java/com/codesoom/assignment/httpHandlers/TaskHttpHandler.java
@@ -8,29 +8,29 @@ import com.codesoom.assignment.httpHandlers.responses.ResponseBadRequest;
 import com.codesoom.assignment.httpHandlers.responses.ResponseConflict;
 import com.codesoom.assignment.httpHandlers.responses.ResponseNotFound;
 import com.codesoom.assignment.repository.TaskRepository;
+import com.codesoom.assignment.routers.AppRouter;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
 import java.io.IOException;
-import java.net.URI;
 
 public class TaskHttpHandler implements HttpHandler {
 
     private TaskRepository taskRepository = new TaskRepository();
+    private AppRouter appRouter = new AppRouter();
 
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        URI requestURI = exchange.getRequestURI();
-        String path = requestURI.getPath();
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        TaskController taskController = new TaskController(taskRepository);
+        TasksController tasksController = new TasksController(taskRepository, objectMapper);
+        appRouter.register(taskController);
+        appRouter.register(tasksController);
 
         try {
-            if (path.equals("/tasks")) {
-                TasksController tasksController = new TasksController(exchange, taskRepository);
-                tasksController.handleRequest();
-            } else if (path.startsWith("/tasks/")) {
-                TaskController taskController = new TaskController(exchange, taskRepository);
-                taskController.handleRequest();
-            }
+            appRouter.handleRequest(exchange);
         } catch (TaskNotFoundException e) {
             new ResponseNotFound(exchange).send(HttpStatus.NOT_FOUND.name());
         } catch (IllegalArgumentException e) {
