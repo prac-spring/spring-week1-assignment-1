1b96689769b41ff4f472e925f339aefd486c9a27
humblEgo <injun.woo30000@gmail.com>
2021-05-14 00:57:33 +0900

    refactor: Task 객체에 toObject, toJson 메서드 추가

--- app/src/main/java/com/codesoom/assignment/models/Task.java
+++ app/src/main/java/com/codesoom/assignment/models/Task.java
@@ -1,8 +1,18 @@
 package com.codesoom.assignment.models;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
 public class Task {
+    final static ObjectMapper objectMapper = new ObjectMapper();
     private Long id;
     private String title;
+
+    public Task() {}
 
     public Long getId() {
         return id;
@@ -18,6 +28,17 @@ public class Task {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public static Task toObject(String requestBody) throws JsonProcessingException {
+        return objectMapper.readValue(requestBody, Task.class);
+    }
+
+    public String toJson() throws IOException {
+        OutputStream outputStream = new ByteArrayOutputStream();
+        objectMapper.writeValue(outputStream, this);
+
+        return outputStream.toString();
     }
 
     @Override
