1027761676a25a660325efc000c488d297daffdd
humblEgo <injun.woo30000@gmail.com>
2021-05-14 01:00:11 +0900

    refactor: Repository, Parser 객체와 책임 분산

--- app/src/main/java/com/codesoom/assignment/httpHandlers/TaskHttpHandler.java
+++ app/src/main/java/com/codesoom/assignment/httpHandlers/TaskHttpHandler.java
@@ -8,26 +8,20 @@ import com.codesoom.assignment.httpHandlers.responses.ResponseBadRequest;
 import com.codesoom.assignment.httpHandlers.responses.ResponseConflict;
 import com.codesoom.assignment.httpHandlers.responses.ResponseNotFound;
 import com.codesoom.assignment.models.Task;
-import com.fasterxml.jackson.core.JsonProcessingException;
+import com.codesoom.assignment.parser.TasksRequestParser;
+import com.codesoom.assignment.repository.TaskRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
-import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.URI;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class TaskHttpHandler implements HttpHandler {
 
-    final private List<Task> tasks = new ArrayList<>();
-    private long taskId = 0;
+    private TaskRepository taskRepository = new TaskRepository();
     private ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
@@ -55,24 +49,22 @@ public class TaskHttpHandler implements HttpHandler {
     }
 
     private HttpResponseData processTask(HttpExchange exchange) throws IOException {
-        Task task = getTask(exchange);
+        TasksRequestParser parser = new TasksRequestParser(exchange);
         HttpStatus status = HttpStatus.OK;
         String content = "";
-        Task updateRequestedContent;
-
+        long id = parser.parseIdInPath();
         HttpMethod method = HttpMethod.valueOf(exchange.getRequestMethod());
         switch (method) {
             case GET:
-                content = toJson(task);
+                content = taskRepository.getTask(id).toJson();
                 break;
             case PUT:
             case PATCH:
-                updateRequestedContent = toTask(parseRequestBody(exchange));
-                task.setTitle(updateRequestedContent.getTitle());
-                content = toJson(task);
+                String requestBody = parser.parseRequestBody();
+                content = taskRepository.updateTask(id, Task.toObject(requestBody)).toJson();
                 break;
             case DELETE:
-                tasks.remove(task);
+                taskRepository.removeTask(id);
                 status = HttpStatus.NO_CONTENT;
                 break;
             default:
@@ -81,30 +73,20 @@ public class TaskHttpHandler implements HttpHandler {
         return new HttpResponseData(status, content);
     }
 
-    private Task getTask(HttpExchange exchange) throws TaskNotFoundException {
-        URI requestURI = exchange.getRequestURI();
-        String path = requestURI.getPath();
-        long taskId = Long.parseLong(path.substring("/tasks/".length()));
-        Task task = findTaskById(taskId);
-        if (task == null) {
-            throw new TaskNotFoundException();
-        }
-        return task;
-    }
-
     private HttpResponseData processTasks(HttpExchange exchange) throws IOException {
+        TasksRequestParser parser = new TasksRequestParser(exchange);
         HttpStatus status = HttpStatus.OK;
         String content = "";
 
         HttpMethod method = HttpMethod.valueOf(exchange.getRequestMethod());
         switch (method) {
             case GET:
-                content = toJson(this.tasks);
+                content = toJson(taskRepository.getAllTasks());
                 break;
             case POST:
-                Task task = addTask(parseRequestBody(exchange));
+                Task newTask = taskRepository.addTask(parser.parseRequestBody());
                 status = HttpStatus.CREATED;
-                content = toJson(task);
+                content = newTask.toJson();
                 break;
             default:
                 break;
@@ -126,44 +108,7 @@ public class TaskHttpHandler implements HttpHandler {
         responseBody.close();
     }
 
-    private String parseRequestBody(HttpExchange exchange) {
-        InputStream inputStream = exchange.getRequestBody();
-        return new BufferedReader(new InputStreamReader(inputStream))
-                .lines()
-                .collect(Collectors.joining("\n"));
-    }
-
-    private Task addTask(String requestBody) throws JsonProcessingException {
-        if (requestBody.isBlank()) {
-            throw new IllegalArgumentException("requestBody가 비었습니다.");
-        }
-
-        Task task = generateTask(requestBody);
-        this.tasks.add(task);
-        return task;
-    }
-
-    private Task generateTask(String requestBody) throws JsonProcessingException {
-        Task task = toTask(requestBody);
-        task.setId(generateTaskId());
-        return task;
-    }
-
-    private Long generateTaskId() {
-        return this.taskId += 1;
-    }
-
-    private Task findTaskById(long id) {
-        return this.tasks.stream()
-                .filter(task -> task.getId().equals(id))
-                .findFirst().orElse(null);
-    }
-
-    private Task toTask(String content) throws JsonProcessingException {
-        return objectMapper.readValue(content, Task.class);
-    }
-
-    private String toJson(Object object) throws IOException {
+    public String toJson(Object object) throws IOException {
         OutputStream outputStream = new ByteArrayOutputStream();
         objectMapper.writeValue(outputStream, object);
 
